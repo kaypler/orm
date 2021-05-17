5b493d80fca73d670a81130b948860caa9b64cac
kaypler <xwd0823@163.com>
2021-05-17 10:55:08 +0800

    finish.

--- src/store/record.ts
+++ src/store/record.ts
@@ -1,10 +1,10 @@
 const http = require('../utils/http');
 
 class Record<T> {
-    readonly id: string
-    readonly type: string
-    #attrs:  T
-    [propName: string]: any
+    readonly id: string;
+    readonly type: string;
+    #attrs:  T;
+    [propName: string]: any;
     // type: string = ''
     // name: string = ''
     // begin_time: number = 0
@@ -16,8 +16,10 @@ class Record<T> {
 
     constructor(attrs: T) {
         this.#attrs = attrs;
-        for (let key in attrs) {
-            this[key] = attrs[key] as any;
+        for (const key in attrs) {
+            if (attrs.hasOwnProperty(key)) {
+                this[key] = attrs[key] as any;
+            }
         }
     }
 
@@ -31,8 +33,10 @@ class Record<T> {
 
     /** 放弃所有未保存的记录更改 */
     rollBack() {
-        for (let key in this.#attrs) {
-            this[key] = this.#attrs[key] as any;
+        for (const key in this.#attrs) {
+            if (this.#attrs.hasOwnProperty(key)) {
+                this[key] = this.#attrs[key] as any;
+            }
         }
     }
 
@@ -41,8 +45,10 @@ class Record<T> {
      * @returns 返回保存请求是否完成
      */
     save() {
-        for (let key in this.#attrs) {
-            this.#attrs[key] = this[key] as any;
+        for (const key in this.#attrs) {
+            if (this.#attrs.hasOwnProperty(key)) {
+                this.#attrs[key] = this[key] as any;
+            }
         }
         return http.put(`/${this.type}/${this.id}`, this.#attrs);
     }
